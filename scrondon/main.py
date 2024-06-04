64ff400573d3f563ad2831d1d8982da90c18d058
novusB <155844237+novusB@users.noreply.github.com>
2024-06-03 21:03:30 -0400

    main

---
+++ scrondon/main.py
@@ -0,0 +1,9 @@
+from redbot.core import commands
+
+class scrondon(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="scrondon")
+    async def scrondon(self, ctx):
+        await ctx.send("Airman Scrondon reporting for duty!")
