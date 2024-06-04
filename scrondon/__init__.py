3d4eafe38c90f74672a26761275691478abae962
novusB <155844237+novusB@users.noreply.github.com>
2024-06-03 21:17:09 -0400

    adios!

--- scrondon/__init__.py
+++
@@ -1,5 +0,0 @@
-from .main import scrondon
-
-
-async def setup(bot):
-    await bot.add_cog(scrondon(bot))
--- scrondon/main.py
+++
@@ -1,9 +0,0 @@
-from redbot.core import commands
-
-class scrondon(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(name="scrondon")
-    async def mycom(self, ctx):
-        await ctx.send("Airman Scrondon reporting for duty!")
