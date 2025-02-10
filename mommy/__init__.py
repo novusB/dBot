cb7bf2569bf9fc57cea6026a46f548a16b555e3c
novusB <155844237+novusB@users.noreply.github.com>
2025-02-09 21:54:59 -0500

    Delete directory

--- mommy/__init__.py
+++
@@ -1,5 +0,0 @@
-from .main import mommy
-
-
-async def setup(bot):
-    await bot.add_cog(mommy(bot))
--- mommy/main.py
+++
@@ -1,9 +0,0 @@
-from redbot.core import commands
-
-class mommy(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(name="mommy")
-    async def mycom(self, ctx):
-        await ctx.send("novusB loves mommies! Do you love mommies too?")
